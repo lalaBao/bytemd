9ece55b86869136bb927b6ebd66c2a70d64ff9a7
pd4d10 <pd4d10@users.noreply.github.com>
2020-03-10 18:01:21 +0000

    Automated deployment: Tue Mar 10 18:01:21 UTC 2020 a70838ad9827d3206d431d181c0cb3b55170206e

---
+++ build/module/main.js
@@ -0,0 +1,1 @@
+export{a as default}from"./main-5c49ae20.js";
---
+++ build/nomodule/main.js
@@ -0,0 +1,1 @@
+System.register(["./main-e3e198e2.js"],(function(e){"use strict";return{setters:[function(t){e("default",t.a)}],execute:function(){}}}));
